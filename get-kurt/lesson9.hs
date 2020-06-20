ad307d144340452b48770e688888a84ce8159eeb
Hakimba <hakim.baaloudj@gmail.com>
2020-06-20 16:22:08 +0200

    lesson 9 finished

--- get-kurt/lesson9.hs
+++ get-kurt/lesson9.hs
@@ -1,4 +1,7 @@
 -- Lesson 9 -> Higher order functions
+
+import Data.Char as C
+
 add3ToAll :: [Int] -> [Int]
 add3ToAll [] = []
 add3ToAll (x:xs) = (3+x) : add3ToAll xs
@@ -39,7 +42,37 @@ myFoldr f init (x:xs) = f x rightResult
   where rightResult = myFoldr f init xs
 
 -- >>> myFold (-) 0 [1..5]
--- -15 ->  ((((1 - 2) - 3) - 4) - 5)
+-- -15 ->  (((((0 - 1) - 2) - 3) - 4) - 5)
+-- idea of the recursion pattern : function ((-) ancient val) rest
 
 -- >>> myFoldr (-) 0 [1..5]
--- 3 -> 1 - (2 - (3 - (4 - 5)))
+-- 3 -> 0 - (1 - (2 - (3 - (4 - 5))))
+-- idea of the recursion pattern : (-) neutral (function val rest))
+
+-- Q9.1
+
+myElem x [] = False
+myElem x xs = (length $  filter (== x) xs) > 0
+
+-- >>> myElem 4 [1..3]
+-- False
+-- >>> myElem 4 [1..10]
+-- True
+
+-- Q9.2
+
+isPalindrome sentence = reverse normalized == normalized
+  where
+    normalized = map (toLower) $ filter (/= ' ') sentence
+
+-- >>> isPalindrome "A man a plan a canal Panama"
+-- True
+
+
+-- Q9.3
+harmonic 1 = 1
+harmonic n = 1/n + harmonic (n-1)
+
+-- >>> harmonic 4
+-- 2.083333333333333
+
